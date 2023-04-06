542b600948e0aaa25f352c027dc52c524c881b23
dannytayade1495 <dannytayade1495@gmail.com>
2023-04-06 14:16:29 +0530

    adding SESSION feature in mvc

--- springmvc/src/main/java/com/jspiders/springmvc/controller/EmployeeController.java
+++ springmvc/src/main/java/com/jspiders/springmvc/controller/EmployeeController.java
@@ -2,12 +2,16 @@ package com.jspiders.springmvc.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.jspiders.springmvc.pojo.AdminPOJO;
 import com.jspiders.springmvc.pojo.EmployeePOJO;
@@ -25,128 +29,178 @@ public class EmployeeController {
 
 	// Home Controller
 	@GetMapping("/home")
-	public String home() {
-		return "Home";
+	public String home(@SessionAttribute(name = "login", required = false) AdminPOJO login, ModelMap map) {
+		if (login != null) {
+			return "Home";
+		}
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
 	}
 
 	// Add Page Controller
 	@GetMapping("/add")
-	public String addPage() {
-		return "Add";
+	public String addPage(@SessionAttribute(name = "login", required = false) AdminPOJO login, ModelMap map) {
+		if (login != null) {
+			return "Add";
+		}
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
+
 	}
 
 	// Add Data Controller
 	@PostMapping("/add")
-	public String add(@RequestParam String name, @RequestParam String email, @RequestParam long contact,
-			@RequestParam String designation, @RequestParam double salary, ModelMap map) {
-		EmployeePOJO employee = service.addEmployee(name, email, contact, designation, salary);
-		if (employee != null) {
-			// Success response
-			map.addAttribute("msg", "Data added successfully..!!");
+	public String add(@SessionAttribute(name = "login", required = false) AdminPOJO login, @RequestParam String name,
+			@RequestParam String email, @RequestParam long contact, @RequestParam String designation,
+			@RequestParam double salary, ModelMap map) {
+		if (login != null) {
+			EmployeePOJO employee = service.addEmployee(name, email, contact, designation, salary);
+			if (employee != null) {
+				// Success response
+				map.addAttribute("msg", "Data added successfully..!!");
+				return "Add";
+			}
+			// Failure response
+			map.addAttribute("msg", "Data not added successfully..!!");
 			return "Add";
 		}
-		// Failure response
-		map.addAttribute("msg", "Data not added successfully..!!");
-		return "Add";
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
+
 	}
 
 	// Search Page Controller
 	@GetMapping("/search")
-	public String searchPage() {
-		return "Search";
+	public String searchPage(@SessionAttribute(name = "login", required = false) AdminPOJO login, ModelMap map) {
+		if (login != null) {
+			return "Search";
+		}
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
 	}
 
 	// Search Data Controller
 	@PostMapping("/search")
-	public String search(@RequestParam int id, ModelMap map) {
-		EmployeePOJO employee = service.search(id);
-		if (employee != null) {
-			// Success Response
-			map.addAttribute("msg", "Employee record found successfully..!!");
-			map.addAttribute("emp", employee);
+	public String search(@SessionAttribute(name = "login", required = false) AdminPOJO login, @RequestParam int id,
+			ModelMap map) {
+		if (login != null) {
+			EmployeePOJO employee = service.search(id);
+			if (employee != null) {
+				// Success Response
+				map.addAttribute("msg", "Employee record found successfully..!!");
+				map.addAttribute("emp", employee);
+				return "Search";
+			}
+			// Failure Response
+			map.addAttribute("msg", "Employee record not found..!!");
 			return "Search";
 		}
-		// Failure Response
-		map.addAttribute("msg", "Employee record not found..!!");
-		return "Search";
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
 	}
 
 	// Remove Page Controller
 	@GetMapping("/remove")
-	public String remove(ModelMap map) {
-		List<EmployeePOJO> employees = service.searchAllEmployees();
-		map.addAttribute("empList", employees);
-		return "Remove";
+	public String remove(@SessionAttribute(name = "login", required = false) AdminPOJO login, ModelMap map) {
+		if (login != null) {
+			List<EmployeePOJO> employees = service.searchAllEmployees();
+			map.addAttribute("empList", employees);
+			return "Remove";
+		}
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
 	}
 
 	// Remove Data Controller
 	@PostMapping("/remove")
-	public String remove(@RequestParam int id, ModelMap map) {
-		EmployeePOJO employee = service.search(id);
-		if (employee != null) {
-			// Success Response
-			service.removeEmployee(id);
+	public String remove(@SessionAttribute(name = "login", required = false) AdminPOJO login, @RequestParam int id,
+			ModelMap map) {
+		if (login != null) {
+			EmployeePOJO employee = service.search(id);
+			if (employee != null) {
+				// Success Response
+				service.removeEmployee(id);
+				List<EmployeePOJO> employees = service.searchAllEmployees();
+				map.addAttribute("empList", employees);
+				map.addAttribute("msg", "Employee removed successfully..!!");
+				return "Remove";
+			}
+			// Failure Response
 			List<EmployeePOJO> employees = service.searchAllEmployees();
 			map.addAttribute("empList", employees);
-			map.addAttribute("msg", "Employee removed successfully..!!");
+			map.addAttribute("msg", "Employee data does not exist..!!");
 			return "Remove";
 		}
-		// Failure Response
-		List<EmployeePOJO> employees = service.searchAllEmployees();
-		map.addAttribute("empList", employees);
-		map.addAttribute("msg", "Employee data does not exist..!!");
-		return "Remove";
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
 	}
 
 	// Update Page Controller
 	@GetMapping("/update")
-	public String updatePage(ModelMap map) {
-		List<EmployeePOJO> employees = service.searchAllEmployees();
-		map.addAttribute("empList", employees);
-		return "Update";
-	}
-
-	// Update Form Controller
-	@PostMapping("/update")
-	public String updateForm(@RequestParam int id, ModelMap map) {
-		EmployeePOJO employee = service.search(id);
-		if (employee != null) {
-			// Success response
-			map.addAttribute("emp", employee);
-			return "Update";
-		}
-		// Failure response
-		map.addAttribute("msg", "Employee record not found..!!");
-		List<EmployeePOJO> employees = service.searchAllEmployees();
-		map.addAttribute("empList", employees);
-		return "Update";
-	}
-
-	// Update Data Controller
-	@PostMapping("/updateData")
-	public String update(@RequestParam int id, @RequestParam String name, @RequestParam String email,
-			@RequestParam long contact, @RequestParam String designation, @RequestParam double salary, ModelMap map) {
-		EmployeePOJO employee = service.updateEmployee(id, name, email, contact, designation, salary);
-		if (employee != null) {
-			// Success response
-			map.addAttribute("msg", "Employee record updated successfully..!!");
+	public String updatePage(@SessionAttribute(name = "login", required = false) AdminPOJO login, ModelMap map) {
+		if (login != null) {
 			List<EmployeePOJO> employees = service.searchAllEmployees();
 			map.addAttribute("empList", employees);
 			return "Update";
 		}
-		// Failure response
-		map.addAttribute("msg", "Employee record not updated..!!");
-		List<EmployeePOJO> employees = service.searchAllEmployees();
-		map.addAttribute("empList", employees);
-		return "Update";
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
+	}
+
+	// Update Form Controller
+	@PostMapping("/update")
+	public String updateForm(@SessionAttribute(name = "login", required = false) AdminPOJO login, @RequestParam int id,
+			ModelMap map) {
+		if (login != null) {
+			EmployeePOJO employee = service.search(id);
+			if (employee != null) {
+				// Success response
+				map.addAttribute("emp", employee);
+				return "Update";
+			}
+			// Failure response
+			map.addAttribute("msg", "Employee record not found..!!");
+			List<EmployeePOJO> employees = service.searchAllEmployees();
+			map.addAttribute("empList", employees);
+			return "Update";
+		}
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
+	}
+
+	// Update Data Controller
+	@PostMapping("/updateData")
+	public String update(@SessionAttribute(name = "login", required = false) AdminPOJO login, @RequestParam int id,
+			@RequestParam String name, @RequestParam String email, @RequestParam long contact,
+			@RequestParam String designation, @RequestParam double salary, ModelMap map) {
+		if (login != null) {
+			EmployeePOJO employee = service.updateEmployee(id, name, email, contact, designation, salary);
+			if (employee != null) {
+				// Success response
+				map.addAttribute("msg", "Employee record updated successfully..!!");
+				List<EmployeePOJO> employees = service.searchAllEmployees();
+				map.addAttribute("empList", employees);
+				return "Update";
+			}
+			// Failure response
+			map.addAttribute("msg", "Employee record not updated..!!");
+			List<EmployeePOJO> employees = service.searchAllEmployees();
+			map.addAttribute("empList", employees);
+			return "Update";
+		}
+		map.addAttribute("msg", "Please login to proceed..!!");
+		return "Login";
 	}
 
 	// Login Controller
 	@PostMapping("/login")
-	public String login(@RequestParam String username, @RequestParam String password, ModelMap map) {
+	public String login(HttpServletRequest request, @RequestParam String username, @RequestParam String password,
+			ModelMap map) {
 		AdminPOJO admin = adminService.login(username, password);
 		if (admin != null) {
 			// Success response
+			HttpSession session = request.getSession();
+			session.setAttribute("login", admin);
 			return "Home";
 		}
 		// Failure response
@@ -156,8 +210,9 @@ public class EmployeeController {
 
 	// Logout Controller
 	@GetMapping("/logout")
-	public String logout(ModelMap map) {
+	public String logout(HttpSession session, ModelMap map) {
 		map.addAttribute("msg", "Logged out successfully");
+		session.invalidate();
 		return "Login";
 	}
 
